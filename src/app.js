a0d2b949c490ed0974e10360b163c3bf4ee4a133
Rohit Yadav <rohityadav85801@gmail.com>
2025-07-14 08:24:54 +0000

    solved error

--- src/app.js
+++ src/app.js
@@ -15,19 +15,6 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
-app.options("*", cors(corsOptions)); // Handle preflight requests
-
-//  Additional headers to ensure compatibility (especially on Render)
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://snipshareapp.netlify.app");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(express.json());
 app.use(cookieParser());
